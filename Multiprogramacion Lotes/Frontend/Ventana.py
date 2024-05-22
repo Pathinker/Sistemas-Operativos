fc19768944fb225f08e1a05c6c71438b9a6c406b
Daniel <159450603+DarchoG@users.noreply.github.com>
2024-05-22 16:45:01 -0600

    Update Ventana.py

--- Multiprogramacion Lotes/Frontend/Ventana.py
+++ Multiprogramacion Lotes/Frontend/Ventana.py
@@ -8,12 +8,20 @@ class Ventana:
         self.app = CTk()
         self.ventanaAncho = self.obtenerAncho(self.app, 70)
         self.ventanaLargo = self.obtenerLargo(self.app, 70)
-        #self.app.afeter(201, lambda : self.app.iconbitmap("Frontend/Icono.ico"))
+        self.app.after(201, lambda : self.app.iconbitmap("Frontend/Imagenes/Icono.ico"))
+        self.app.title("Multiprogramación Lotes")
 
         self.app.geometry(f"{self.ventanaAncho}x{self.ventanaLargo}")
-        self.app.mainloop()
 
+        #Paleta Colores
+
+        self.colorFondo = '#373739'
+        self.primerGris = "#19191a"
+        self.segundoGris = "#4A4A4D"
+        self.textoGris = "#c6c6c6"
+    
         self.generarFrames(self.app)
+        self.app.mainloop()
         
     def obtenerAncho(self, Ventana, Proporcion):
 
@@ -51,8 +59,8 @@ class Ventana:
                                 width = segundoFrameAncho,
                                 height = segundoFrameLargo)
         
-        primerFrame.grid(row = 0, column = 0, sticky = "nsew")
-        segundoFrame.grid(row = 1, column = 0, sticky = "nsew")
+        primerFrame.grid(row = 0, column = 0, sticky = "nsew", padx = 10, pady = 5)
+        segundoFrame.grid(row = 1, column = 0, sticky = "nsew", padx = 10, pady = 5)
         
         self.generarContenido(primerFrame, primerFrameAncho, primerFrameLargo)
         self.cajaTexto(segundoFrame, segundoFrameAncho, segundoFrameLargo)
@@ -62,46 +70,187 @@ class Ventana:
         Frame.rowconfigure(0, weight = 1)
         Frame.columnconfigure(0, weight = 1)
         Frame.columnconfigure(1, weight = 1)
-        Frame.columnconfigure(1, weight = 1)
+        Frame.columnconfigure(2, weight = 1)
 
         contenedoresAncho = self.obtenerEscala(Ancho, 33)
         contenedoresLargo = self.obtenerEscala(Largo, 100)
 
-        Nuevos = CTkScrollableFrame(master = Frame,
-                                    width = contenedoresAncho,
-                                    height = contenedoresLargo
-                                    )
+        Nuevos = CTkFrame(master = Frame,
+                       width = contenedoresAncho,
+                       height = contenedoresLargo,
+                       fg_color = self.colorFondo
+                       )
         
         Pendientes = CTkFrame(master = Frame,
                               width = contenedoresAncho,
                               height = contenedoresLargo)
 
-        Terminados = CTkScrollableFrame(master = Frame,
-                                    width = contenedoresAncho,
-                                    height = contenedoresLargo
-                                    )
+        Terminados = CTkFrame(master = Frame,
+                              width = contenedoresAncho,
+                              height = contenedoresLargo,
+                              fg_color = self.colorFondo
+                              )
         
         Nuevos.grid(row = 0, column = 0, sticky = "nsew")
-        Pendientes.grid(row = 1, column = 0, sticky = "nsew", padx = 20)
-        Terminados.grid(row = 2, column = 0, sticky = "nsew")
+        Pendientes.grid(row = 0, column = 1, sticky = "nsew", padx = 20)
+        Terminados.grid(row = 0, column = 2, sticky = "nsew")
 
-        self.generarPendientes(Pendientes, Ancho, Largo)
+        self.generarNuevos(Nuevos, contenedoresAncho, contenedoresLargo)
+        self.generarPendientes(Pendientes, contenedoresAncho, contenedoresLargo)
+        self.generarTerminados(Terminados, contenedoresAncho, contenedoresLargo)
 
-    def generarPendientes(Frame, Ancho, Largo):
+    def generarNuevos(self, Frame, Ancho, Largo):
+
+        Frame.rowconfigure(0, weight = 0)
+        Frame.rowconfigure(1, weight = 1)
+        Frame.columnconfigure(0, weight = 1)    
+
+        tituloAncho = self.obtenerEscala(Ancho, 100)
+        tituloLargo = self.obtenerEscala(Largo, 5)
+
+        nuevosAncho = self.obtenerEscala(Ancho, 100)
+        nuevosLargo = self.obtenerEscala(Largo, 95)
+
+        Imagen = CTkImage(light_image = Image.open("Frontend/Imagenes/Nuevos.png"),
+                          dark_image = Image.open("Frontend/Imagenes/Nuevos.png"),
+                          size = (16, 16))
+        
+        titulo = CTkLabel(master = Frame,
+                          width = tituloAncho,
+                          height = tituloLargo,
+                          fg_color = self.primerGris,
+                          text = " Nuevos",
+                          font = ("Helvetica", 14),
+                          anchor = "w",
+                          image = Imagen,
+                          compound = "left")
+        
+        nuevosContenedor = CTkScrollableFrame(master = Frame,
+                                              width = nuevosAncho,
+                                              height = nuevosLargo,
+                                              fg_color = self.colorFondo)
+        
+        titulo.grid(row = 0, column = 0, sticky = "nsew")
+        nuevosContenedor.grid(row = 1, column = 0, sticky = "nsew")
+
+    def generarPendientes(self, Frame, Ancho, Largo):
 
         Frame.rowconfigure(0, weight = 1)
         Frame.rowconfigure(1, weight = 1)
         Frame.columnconfigure(0, weight = 1)
 
-        #Listos = CTkFrame     
+        listosAncho = self.obtenerEscala(Ancho, 100)
+        listosLargo = self.obtenerEscala(Largo, 60)
 
-    def cajaTexto(Frame, Ancho, Largo):
+        ejecucionAncho = self.obtenerEscala(Ancho, 100)
+        ejecucionLargo = self.obtenerEscala(Largo, 40)
 
-        Frame.rowconfigure(0, weight = 1)
+        Listos = CTkFrame(master = Frame,
+                          width = listosAncho,
+                          height = listosLargo,
+                          ) 
+        
+        Ejecucion = CTkFrame(master = Frame,
+                             width = ejecucionAncho,
+                             height = ejecucionLargo,
+                             )
+        
+        Listos.grid(row = 0, column = 0, sticky = "nsew")
+        Ejecucion.grid(row = 1, column = 0, sticky = "nsew")
+
+        self.generarListos(Listos, listosAncho, listosLargo)
+        self.generarEjecucion(Ejecucion, ejecucionAncho, ejecucionLargo)
+
+    def generarListos(self, Frame, Ancho, Largo):
+
+        pass
+
+    def generarEjecucion(self, Frame, Ancho, Largo):
+
+        pass
+
+    def generarTerminados(self, Frame, Ancho, Largo):
+
+        Frame.rowconfigure(0, weight = 0)
+        Frame.rowconfigure(1, weight = 1)
+        Frame.columnconfigure(0, weight = 1)    
+
+        tituloAncho = self.obtenerEscala(Ancho, 100)
+        tituloLargo = self.obtenerEscala(Largo, 5)
+
+        nuevosAncho = self.obtenerEscala(Ancho, 100)
+        nuevosLargo = self.obtenerEscala(Largo, 95)
+
+        Imagen = CTkImage(light_image = Image.open("Frontend/Imagenes/Terminados.png"),
+                          dark_image = Image.open("Frontend/Imagenes/Terminados.png"),
+                          size = (16, 16))
+        
+        titulo = CTkLabel(master = Frame,
+                          width = tituloAncho,
+                          height = tituloLargo,
+                          fg_color = self.primerGris,
+                          text = " Terminados",
+                          font = ("Helvetica", 14),
+                          anchor = "w",
+                          image = Imagen,
+                          compound = "left")
+        
+        nuevosContenedor = CTkScrollableFrame(master = Frame,
+                                              width = nuevosAncho,
+                                              height = nuevosLargo,
+                                              fg_color = self.colorFondo)
+        
+        titulo.grid(row = 0, column = 0, sticky = "nsew")
+        nuevosContenedor.grid(row = 1, column = 0, sticky = "nsew")
+        
+    def cajaTexto(self, Frame, Ancho, Largo):
+
+        Frame.rowconfigure(0, weight = 0)
+        Frame.rowconfigure(1, weight = 1)
         Frame.columnconfigure(0, weight = 1)
 
-        Contenido = CTkTextbox(master = Frame,
-                               width = Ancho,
-                               height = Largo)
+        tituloAncho = self.obtenerEscala(Ancho, 100)
+        tituloLargo = self.obtenerEscala(Largo, 10)
 
-        Contenido.grid(row = 0, column = 0, sticky = "nsew")    
+        contenidoAncho = self.obtenerEscala(Ancho, 100)
+        contenidoLargo = self.obtenerEscala(Largo, 90)
+
+        Imagen = CTkImage(light_image = Image.open("Frontend/Imagenes/Terminal.png"),
+                          dark_image = Image.open("Frontend/Imagenes/Terminal.png"),
+                          size = (16, 16))
+
+        Titulo = CTkLabel(master = Frame,
+                          width = tituloAncho,
+                          height = tituloLargo,
+                          text = " Terminal",
+                          font = ("Helvetica", 14),
+                          fg_color = self.primerGris,
+                          anchor = "w",
+                          image = Imagen,
+                          compound = "left")
+
+        Contenido = CTkTextbox(master = Frame,
+                               width = contenidoAncho,
+                               height = contenidoLargo,
+                               fg_color = self.colorFondo,
+                               text_color = "White",
+                               corner_radius = 0,
+                               font=("Consolas", 16))
+        
+        Contenido.insert("1.0", "Escriba Teclas Entrada\n\n")
+        Contenido.insert("3.0", "E: Interrupcion  Entrada/Salida\n")
+        Contenido.insert("4.0", "W: Error\n")
+        Contenido.insert("5.0", "P: Pausa\n")
+        Contenido.insert("6.0", "C: Continuar\n\n")
+        Contenido.insert("8.0", "Las teclas seran detectadas al presionar [Enter]\n\n")
+
+        Contenido.bind("<Return>", lambda event: self.Enter(event))
+
+        Titulo.grid(row = 0, column = 0, sticky = "nsew")
+        Contenido.grid(row = 1, column = 0, sticky = "nsew")    
+
+    def Enter(self, Caracter):
+
+        print(Caracter)
+
+        #Implementar las opciones con el backend.   
